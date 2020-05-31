3d0200feae279b674b3997c9c657febc428d6460
Dimitris Kagialis <kagialisd@gmail.com>
2020-05-31 21:54:18 +0300

    v: 0.1

---
+++ app/profile_output.py
@@ -0,0 +1,57 @@
+import sys
+
+sys.path.append("../")
+from app.lib.logsys import Log
+from app.lib.filesystem import Filesystem
+from app.lib.system import System
+
+def main():
+    Log.info('---------------------')
+    Log.info('Profiler task Starts')
+    Log.info('---------------------')
+    files = Filesystem.getFiles()
+    Log.info("List of afas files:")
+    for file in files:
+        Log.info(file)
+    Log.info('Merging the files with MUSCLE profile option')
+    filesNumber = len(files)
+    count = 0
+    while count < filesNumber:
+        if count == 0:
+            Log.info('Profiling the first two files..')
+            print(files[count], files[count + 1], '../data/profile/temp-' + str(count+1) + '.afas')
+            (ret, out, err) = muscle(files[count], files[count + 1], '../data/profile/temp-' + str(count+1) + '.afas')
+            Log.info("return, {}".format(ret))
+            Log.info("output, {}".format(out))
+            Log.error("error, {}".format(err))
+        elif filesNumber == count + 1:
+            Log.info('Profiling the last file..')
+            print(files[count], '../data/profile/temp-' + str(count - 1) + '.afas','../data/final/result.afas')
+            (ret, out, err) = muscle(files[count], '../data/profile/temp-' + str(count - 1) + '.afas','../data/final/result.afas')
+            Log.info("return, {}".format(ret))
+            Log.info("output, {}".format(out))
+            Log.error("error, {}".format(err))
+        elif filesNumber >= count + 3:
+            Log.info('Profiling a single file..')
+            print(files[count+1], '../data/profile/temp-' + str(count) + '.afas', '../data/profile/temp-' + str(count+1) + '.afas')
+            (ret, out, err) = muscle(files[count+1], '../data/profile/temp-' + str(count) + '.afas', '../data/profile/temp-' + str(count+1) + '.afas')
+            Log.info("return, {}".format(ret))
+            Log.info("output, {}".format(out))
+            Log.error("error, {}".format(err))
+        count = count + 1
+    Log.info('Please see the result in under folder data/final')
+    Log.info('------------------------')
+    Log.info('Profiler task Completes')
+    Log.info('------------------------')
+
+
+def muscle(in1, in2, out):
+    (ret, out, err) = System.command(
+        ['bin/muscle3.8.31_i86darwin64', '-profile',
+         '-in1', in1,
+         '-in2', in2,
+         '-out', out])
+    return ret,out,err
+
+if __name__ == "__main__":
+    main()
---
+++ app/split_input.py
@@ -0,0 +1,65 @@
+import sys
+import math
+
+sys.path.append("../")
+from app.lib.input import Input
+from app.lib.logsys import Log
+
+def main():
+    Log.info('------------------')
+    Log.info('Split task Starts')
+    Log.info('------------------')
+    inputArgs = sys.argv
+    args = inputArgs[1:]
+    filePath, fileName = Input.getValues(args)
+    Log.info('Path:')
+    Log.info(filePath)
+    Log.info('File name:')
+    Log.info(fileName)
+    try:
+        with open(filePath) as f:
+            lines = sum(1 for _ in f)
+    except  EnvironmentError as e:
+        Log.error('Input file error:')
+        Log.error(e)
+        Log.exit()
+    Log.info('Number of sequences:')
+    Log.info(lines/2)
+    Log.info('The dataset is being splittted in files of 50 sequences.')
+    Log.info('Total files:')
+    filesNumber = math.ceil(lines/100)
+    Log.info(filesNumber)
+    filesList = []
+    count = 0
+    Log.info('File names:')
+    while count < filesNumber:
+        count += 1
+        filesList.append('in-'+str(count)+'-'+fileName)
+    for file in filesList:
+        Log.info(file)
+    try:
+        lineCount = 0
+        filesCount = 0
+        input = open(filePath,"r")
+        for line in input:
+            if lineCount % 100 == 0:
+                try:
+                    output.close()
+                except:
+                    Log.info('Start splitting the dataset..')
+                output = open('../data/input/' + filesList[filesCount], "a")
+                filesCount = filesCount + 1
+            output.write(line)
+            lineCount = lineCount + 1
+        input.close()
+        Log.info('Finish splitting the dataset..')
+    except  EnvironmentError as e:
+        Log.error('Unkown error:')
+        Log.error(e)
+        Log.exit()
+    Log.info('---------------------')
+    Log.info('Split task Completes')
+    Log.info('---------------------')
+
+if __name__ == "__main__":
+    main()
